3042494e4cf905c43704d4bcccc9df28645a0c18
chituc <chituclaudia@gmail.com>
2019-02-13 05:08:50 +0200

    Update sliding_1_RF_KNN.py

--- sliding_1_RF_KNN.py
+++ sliding_1_RF_KNN.py
@@ -18,7 +18,7 @@ from sklearn import metrics
 from time import time
 
 def get_files():
-    #get a list of strings with path and file names: ['E:/Github/data\\Room1.csv', ...]
+    #get a list of strings with path and file names: ['D:/Github/data\\Room1.csv', ...]
     fileList = glob.glob("./data/Room*")
     dataSetName = []
     for fileName in fileList:
